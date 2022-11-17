86a748c2c0712a9136a0877b9f48f7391b4533c5
omoflop <29988852+omoflop@users.noreply.github.com>
2022-11-17 10:13:52 -0800

    Initial commit

---
+++ VarElement.cs
@@ -0,0 +1,169 @@
+﻿namespace VarFileLib; 
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public struct VarElement {
+	private Dictionary<string, VarElement> _dictionary;
+	private string _value;
+	private Type _type;
+
+	#region Element methods
+	public void SetElement(string name, VarElement varElement) {
+		if (_type == Type.Var) throw new Exception($"Tried to write element name '{name}' to a var!");
+		_dictionary[name] = varElement;
+	}
+	public VarElement GetElement(string name) {
+		if (_type == Type.Var) throw new Exception($"Tried to read var '{name}' as an element");
+		
+		int period = name.IndexOf('.');
+		return period != -1 ? _dictionary[name[..period]].GetElement(name[(period+1)..]) : _dictionary[name];
+	}
+	private VarElement? GetElementWithoutExploding(string name) {
+		if (_type == Type.Var) throw new Exception($"Tried to read var '{name}' as an element");
+		
+		int period = name.IndexOf('.');
+		if (period != -1) {
+			string subkey = name[..period];
+			if (_dictionary.ContainsKey(subkey)) return _dictionary[subkey].GetElement(name[(period + 1)..]);
+			return null;
+		}
+
+		if (_dictionary.ContainsKey(name)) {
+			return _dictionary[name];
+		}
+
+		return null;
+	}
+	public bool HasElement(string name) {
+		if (_type == Type.Var) throw new Exception($"Tried to read var '{name}' as an element");
+		int period = name.IndexOf('.');
+		return period != -1 ? _dictionary[name[..period]].HasElement(name[(period+1)..]) : _dictionary.ContainsKey(name);
+	}
+	#endregion
+
+	#region Element var methods
+
+	public string ReadElementString(string path) => GetElement(path).ReadString();
+	public string ReadElementStringOrDefault(string path, string def) => HasElement(path) ? ReadElementString(path) : def;
+
+	public int ReadElementInt(string path) => GetElement(path).ReadInt();
+	public int ReadElementIntOrDefault(string path, int def) => HasElement(path) ? ReadElementInt(path) : def;
+	
+	public float ReadElementFloat(string path) => GetElement(path).ReadFloat();
+	public float ReadElementFloatOrDefault(string path, float def) => HasElement(path) ? ReadElementFloat(path) : def;
+
+	public bool ReadElementBool(string path) => GetElement(path).ReadBool();
+	public bool ReadElementBoolOrDefault(string path, bool def) => HasElement(path) ? ReadElementBool(path) : def;
+
+
+	#endregion
+
+	#region Var methods
+	public string ReadString() {
+		MakeSure(Type.Var);
+		return _value;
+	}
+	public string ReadStringOrDefault(string def) {
+		return ReadString() ?? def;
+	}
+
+	public int ReadInt() {
+		MakeSure(Type.Var);
+		if (int.TryParse(_value, out int v)) {
+			return v;
+		}
+
+		throw new Exception($"Failed to read var value as an int, for value: '{_value}'");
+	}
+	public int ReadIntOrDefault(int def) {
+		MakeSure(Type.Var);
+		if (_value != null && int.TryParse(_value, out int v)) {
+			return v;
+		}
+		return def;
+	}
+
+	public float ReadFloat() {
+		MakeSure(Type.Var);
+		if (float.TryParse(_value, out float v)) {
+			return v;
+		}
+
+		throw new Exception($"Failed to read var value as a float, for value: '{_value}'");
+	}
+	public float ReadFloatOrDefault(float def) {
+		MakeSure(Type.Var);
+		if (_value != null && float.TryParse(_value, out float v)) {
+			return v;
+		}
+		return def;
+	}
+
+	public bool ReadBool() {
+		MakeSure(Type.Var);
+		if (bool.TryParse(_value, out bool v)) {
+			return v;
+		}
+
+		throw new Exception($"Failed to read var value as a bool, for value: '{_value}'");
+	}
+	public bool ReadBoolOrDefault(bool def) {
+		MakeSure(Type.Var);
+		if (_value != null && bool.TryParse(_value, out bool v)) {
+			return v;
+		}
+		return def;
+	}
+	#endregion
+
+	private void MakeSure(Type type) {
+		if (_type == Type.Element && type == Type.Var) throw new Exception("Tried to treat an element as a var");
+		if (_type == Type.Var && type == Type.Element) throw new Exception("Tried to treat a var as an element");
+	}
+
+	private enum Type {
+		Element,
+		Var
+	}
+
+	public override string ToString() {
+		StringBuilder builder = new StringBuilder();
+		ToString(builder, 0);
+		return builder.ToString();
+	}
+	
+	private void ToString(StringBuilder builder,int depth) {
+		for (int i = 0; i < depth; i++) {
+			builder.Append("  ");
+		}
+		
+		if (_type == Type.Element) {
+			foreach (var (key, value) in _dictionary) {
+				builder.Append($"[{key}]=");
+				value.ToString(builder, value._type == Type.Var ? 0 : depth+1);
+			}
+		} else if (_type == Type.Var) {
+			builder.Append(_value ?? "null");
+		}
+		
+		builder.AppendLine();
+	}
+
+	public static VarElement CreateElement() {
+		return new VarElement {
+			_dictionary = new Dictionary<string, VarElement>(),
+			_value = null,
+			_type = Type.Element
+		};
+	}
+	
+	public static VarElement CreateVar(string value = null) {
+		return new VarElement {
+			_dictionary = null,
+			_value = value,
+			_type = Type.Var
+		};
+	}
+}
---
+++ VarFileReader.cs
@@ -0,0 +1,103 @@
+﻿namespace VarFileLib; 
+
+using System;
+using System.IO;
+
+public sealed class VarFileReader {
+	public static VarElement Read(string path) {
+		var reader = new VarFileReader();
+		using TextReader textReader = File.OpenText(path);
+		return reader.StartReader(textReader);
+	}
+	
+	private VarFileReader() {}
+
+	private VarElement StartReader(TextReader stream) {
+		VarElement vars = VarElement.CreateElement();
+		ParseString(ref vars, stream.ReadToEnd());
+		
+		return vars;
+	}
+
+	private void ParseString(ref VarElement vars, string str) {
+		int lineNumber = 0;
+		int charIndex = 0;
+		string curLine = str[charIndex..str.IndexOf('\n', 1)];
+		bool lastIteration = false;
+		while(str.Length > charIndex) {
+			curLine = curLine.Trim();
+
+			if (curLine.StartsWith('[')) {
+				int closing = FindClosingBracket(curLine);
+				string name = curLine[1..closing];
+				string lineValue = curLine[(closing + 2)..];
+
+				// If the entire line is a string literal, skip everything and just save the string
+				if (lineValue.StartsWith('\"') && lineValue.EndsWith('\"')) {
+					vars.SetElement(name, VarElement.CreateVar(lineValue[1..^1]));
+				}
+				else // If the line opens with a curly bracket, start doin some recusive shit
+				if (lineValue.StartsWith('{')) {
+					int indx = str.IndexOf('{', charIndex) + 1;
+					int indxclose = str.IndexOf('}', charIndex);
+					string chop = str[indx..indxclose];
+					VarElement element = VarElement.CreateElement();
+					ParseString(ref element, chop);
+					vars.SetElement(name, element);
+
+					charIndex = str.IndexOf('\n', indxclose);
+					if (str.IndexOf('\n', indxclose + 1) == -1) {
+						lastIteration = true;
+						curLine = str[(str.LastIndexOf('\n') + 1)..];
+					}
+					else {
+						curLine = str[(indxclose + 1)..(str.IndexOf('\n', indxclose + 1) - 1)];
+					}
+
+					continue;
+				}
+				else if (lineValue.Contains(':')) {
+					VarElement element = VarElement.CreateElement();
+					string[] vals = lineValue.Split(',');
+					foreach (var val in vals) {
+						var val2 = val.Trim();
+						if (val2.Length == 0) continue;
+						int c = val2.IndexOf(':');
+						string val3 = val2[(c + 1)..];
+						if (val3.StartsWith('"') && val3.EndsWith('"')) val3 = val3[1..^1];
+						
+						element.SetElement(val2[..c], VarElement.CreateVar(val3));
+					}
+					vars.SetElement(name, element);
+				} else {
+					vars.SetElement(name, VarElement.CreateVar(lineValue));
+				}
+			} else if (curLine.Length > 0) {
+				//throw Error("Invalid token");
+			}
+			
+			
+			if (lastIteration) break;
+			charIndex = str.IndexOf('\n', charIndex+1);
+			if (str.IndexOf('\n', charIndex + 1) == -1) {
+				lastIteration = true;
+				curLine = str[(str.LastIndexOf('\n')+1)..];
+			}
+			else {
+				curLine = str[(charIndex + 1)..(str.IndexOf('\n', charIndex + 1) - 1)];
+			}
+
+			lineNumber += 1;
+		}
+	}
+
+	private int FindClosingBracket(string str) {
+		int indx = str.IndexOf("]=", StringComparison.Ordinal);
+		if (indx == -1) throw Error("No closing bracket/assign operator found");
+		return indx;
+	}
+
+	private static Exception Error(string reason) {
+		return new Exception($"Failed to read vars file: {reason}");
+	}
+}
